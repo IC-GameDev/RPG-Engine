a2553f25c801816e37a3499c39f9ed2d15a32f39
David Avedissian <avedissian.david@gmail.com>
2015-11-18 13:08:47 +0000

    Added discussion from other repo

---
+++ doc/MapGen_1/src/mapgen_1/MapGen.java
@@ -0,0 +1,16 @@
+package mapgen_1;
+
+/**
+ *
+ * @author Cyprien
+ */
+public class MapGen {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
